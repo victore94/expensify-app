a457961e9175f88c5f3dbf13314c637f8a28ae0e
Victor Lopez <victorel94@gmail.com>
2020-05-22 15:15:55 -0700

    include webpack in config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
-const path = require('path')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'test') {
-    require('dotenv').config({ path: '.env.test' })
+    require('dotenv').config({ path: '.env.test' });
 } else if (process.env.NODE_ENV === 'development') {
-    require('dotenv').config({ path: '.env.development' })
-
+    require('dotenv').config({ path: '.env.development' });
 }
 
 module.exports = (env) => {
-    const isProduction = env === 'production'
-    const CSSExtract = new ExtractTextPlugin('styles.css')
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+
     return {
         entry: './src/app.js',
         output: {
@@ -24,8 +25,7 @@ module.exports = (env) => {
                 loader: 'babel-loader',
                 test: /\.js$/,
                 exclude: /node_modules/
-            },
-            {
+            }, {
                 test: /\.s?css$/,
                 use: CSSExtract.extract({
                     use: [
@@ -64,6 +64,6 @@ module.exports = (env) => {
             historyApiFallback: true,
             publicPath: '/dist/'
         }
-    }
+    };
 };
 
